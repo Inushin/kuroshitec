2a9793cf5e034b1eb919a59f7baa5032d96bc2af
Aitor Sobera Ortiz de Zárate | Inushin <aitorsobera@gmail.com>
2024-10-22 23:03:55 +0200

    chore: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kuroshitec.com/',
-  integrations: [sitemap()],
+  integrations: [sitemap(), partytown()],
   /*FOR REDIRECTS
   redirects: {
     'OLD_PATH': 'NEW_PATH'
